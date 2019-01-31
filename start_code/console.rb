183cd7845dd5b99c6869d63110c864e7a05e4962
Ethan Radcliffe <ethanr@mac.com>
2019-01-31 14:44:07 +0000

    bluh

--- start_code/console.rb
+++ start_code/console.rb
@@ -4,32 +4,41 @@ require_relative('models/star')
 
 require('pry')
 
+# Movie.delete_all()
+# Star.delete_all()
+# Casting.delete_all()
 
-movie1 = Movie.new({ 'title' => 'Shawshank Redemption', 'genre' => 'Drama'})
-movie1.save
 
-movie2 = Movie.new({ 'title' => 'The Godfather', 'genre' => 'Drama'})
-movie2.save
 
-movie3 = Movie.new({ 'title' => 'The Dark Knight', 'genre' => 'Action'})
-movie3.save
+# movie1 = Movie.new({ 'title' => 'Shawshank Redemption', 'genre' => 'Drama'})
+# movie1.save
+#
+# movie2 = Movie.new({ 'title' => 'The Godfather', 'genre' => 'Drama'})
+# movie2.save
+#
+# movie3 = Movie.new({ 'title' => 'The Dark Knight', 'genre' => 'Action'})
+# movie3.save
 
 star1 = Star.new({ 'first_name' => 'Morgan', 'last_name' => 'Freeman'})
-star1.save
+star1.save()
 
 star2 = Star.new({ 'first_name' => 'Marlon', 'last_name' => 'Brando'})
-star2.save
+star2.save()
 
 star3 = Star.new({ 'first_name' => 'Christian', 'last_name' => 'Bale'})
-star3.save
+star3.save()
 
-casting1 = Casting.new({'movie_id' => movie1.id, 'star_id' => star1.id, 'fee' => 100000})
-
-casting2 = Casting.new({'movie_id' => movie2.id, 'star_id' => star2.id, 'fee' => 200000})
-
-casting3 = Casting.new({'movie_id' => movie3.id, 'star_id' => star3.id, 'fee' => 300000})
-
-casting4 = Casting.new({'movie_id' => movie3.id, 'star_id' => star1.id, 'fee' => 400000})
+# casting1 = Casting.new({'movie_id' => movie1.id, 'star_id' => star1.id, 'fee' => 100000})
+# casting1.save
+#
+# casting2 = Casting.new({'movie_id' => movie2.id, 'star_id' => star2.id, 'fee' => 200000})
+# casting2.save
+#
+# casting3 = Casting.new({'movie_id' => movie3.id, 'star_id' => star3.id, 'fee' => 300000})
+# casting3.save
+#
+# casting4 = Casting.new({'movie_id' => movie3.id, 'star_id' => star1.id, 'fee' => 400000})
+# casting4.save
 
 
 
--- start_code/db/imdb.sql
+++ start_code/db/imdb.sql
@@ -2,13 +2,13 @@ DROP TABLE IF EXISTS casting;
 DROP TABLE IF EXISTS movies;
 DROP TABLE IF EXISTS stars;
 
-CREATE TABLE movies(
-  id SERIAL4 PRIMARY KEY,
-  title VARCHAR(255),
-  genre VARCHAR(255),
-
-
-);
+-- CREATE TABLE movies(
+--   id SERIAL4 PRIMARY KEY,
+--   title VARCHAR(255),
+--   genre VARCHAR(255)
+--
+--
+-- );
 
 CREATE TABLE stars(
   id SERIAL4 PRIMARY KEY,
@@ -16,9 +16,9 @@ CREATE TABLE stars(
   last_name VARCHAR(255)
 );
 
-CREATE TABLE casting(
-  id SERIAL4 PRIMARY KEY,
-  movie_id INT4 REFERENCES movies(id) ON DELETE CASCADE,
-  star_id INT4 REFERENCES stars(id) ON DELETE CASCADE,
-  fee INT4
-  );
+-- CREATE TABLE casting(
+--   id SERIAL4 PRIMARY KEY,
+--   movie_id INT4 REFERENCES movies(id) ON DELETE CASCADE,
+--   star_id INT4 REFERENCES stars(id) ON DELETE CASCADE,
+--   fee INT4
+--   );
